77e45c158f5621c0efbd9a5437c1ae6c75d83f10
suysie <64253520+suysie@users.noreply.github.com>
2020-11-10 21:29:05 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React,{useEffect, useState} from 'react';
+import Recipe from "./Recipe";
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = "2da4af9d";
+  const APP_KEY = "2e2b5a9c2a87fda6e39295bc68829280"
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chickn');
+
+
+  useEffect( () =>{
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = e =>{
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('')
+  };
+
+  return(
+    <div className="App">
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit"> Search</button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories}
+        image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}
+        />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Recipe = ({title,calories,image, ingredients}) => {
+    return(
+        <div className='recipe'>
+            <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>{calories}</p>
+            <img className="image" src={image} alt=""/>
+        </div>
+    )
+}
+
+export default Recipe;
